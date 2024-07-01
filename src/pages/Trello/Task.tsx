a5e97fccb6aa4aa5b234d83ead2e44236b6f844b
Indrasis Datta <indrasis.datta@yahoo.co.in>
2024-07-01 09:11:30 +0530

    Task edit, delete (done) + add (WIP)

--- src/pages/Trello/Task.tsx
+++ src/pages/Trello/Task.tsx
@@ -5,9 +5,10 @@ import { Category, Task, TaskList } from "../../types/Tasks";
 export const TaskComp: FC<{
   catKey: Category;
   taskList: TaskList;
-  editHandler: (task: Task) => void;
-  deleteHandler: (task: Task) => void;
-}> = ({ catKey, taskList, editHandler, deleteHandler }) => {
+  addHandler: (task: Task, catKey: Category) => void;
+  editHandler: (task: Task, catKey: Category) => void;
+  deleteHandler: (task: Task, catKey: Category) => void;
+}> = ({ catKey, taskList, addHandler, editHandler, deleteHandler }) => {
   //   console.log("Debug cat key", catKey, taskList[catKey]);
 
   const [editableTask, setEditableTask] = useState<Task>({
@@ -15,42 +16,101 @@ export const TaskComp: FC<{
     name: "",
   });
 
+  const [newTask, setNewTask] = useState({
+    id: 0,
+    name: "",
+    cat: "",
+  });
+
+  const addTask = () => {
+    setNewTask({
+      id: 0,
+      name: "",
+      cat: catKey,
+    });
+  };
+
   const openEditor = (task: Task) => {
     setEditableTask(task);
   };
 
-  const handleTaskName = (e: ChangeEvent<HTMLInputElement>) => {
-    const taskCopy = structuredClone(editableTask);
-    (taskCopy as Task).name = e.target.value;
-    setEditableTask(taskCopy);
+  const handleTaskName = (
+    e: ChangeEvent<HTMLInputElement>,
+    action: "edit" | "add"
+  ) => {
+    if (action === "edit") {
+      const taskCopy = structuredClone(editableTask);
+      (taskCopy as Task).name = e.target.value;
+      console.log("taskCopy", taskCopy);
+      setEditableTask(taskCopy);
+    } else if (action === "add") {
+      const newTaskCopy = structuredClone(newTask);
+      newTaskCopy.name = e.target.name;
+      setNewTask(newTaskCopy);
+    }
   };
 
   const onSave = () => {
-    editHandler(editableTask);
+    editHandler(editableTask, catKey);
+    setEditableTask({ id: 0, name: "" });
+  };
+
+  const onCancel = () => {
+    setEditableTask({ id: 0, name: "" });
   };
 
   return (
     <div key={categoryMap[catKey]} className={`task-list ${catKey}`}>
       <div className="task-list__header">
         <h4>{categoryMap[catKey]}</h4>
-        <button>Add</button>
+        <button onClick={() => addTask()}>Add</button>
       </div>
       <div className="task-list__body">
-        {taskList[catKey].map((task) => (
-          <p className="task" key={`${categoryMap[catKey]}-${task.id}`}>
-            {editableTask?.id === task.id ? (
-              <>
-                <input value={task.name} onChange={handleTaskName} />
-                <button onClick={onSave}>Save</button>
-              </>
-            ) : (
-              <span>{task.name}</span>
+        {taskList[catKey].map((task, index) => (
+          <>
+            {/* New task */}
+            {index === 0 && newTask.cat === catKey && (
+              <p className="task" key={`${newTask.cat}-${task.id}`}>
+                <>
+                  <input
+                    value={newTask.name}
+                    onChange={() => handleTaskName(e, "add")}
+                  />
+                </>
+                <span>
+                  <button onClick={onSave}>Save</button>&nbsp;
+                  <button onClick={onCancel}>Cancel</button>
+                </span>
+              </p>
             )}
-            <span>
-              <button onClick={() => openEditor(task)}>Edit</button>&nbsp;
-              <button onClick={() => deleteHandler(task)}>Delete</button>
-            </span>
-          </p>
+            {/* // Previously saved task - edit, delete */}
+            <p className="task" key={`${categoryMap[catKey]}-${task.id}`}>
+              {editableTask?.id === task.id ? (
+                <>
+                  <input
+                    value={editableTask.name}
+                    onChange={() => handleTaskName(e, "edit")}
+                  />
+                </>
+              ) : (
+                <span>{task.name}</span>
+              )}
+              {editableTask.id ? (
+                <span>
+                  <button onClick={onSave}>Save</button>&nbsp;
+                  <button onClick={onCancel}>Cancel</button>
+                </span>
+              ) : (
+                <span>
+                  <button onClick={() => openEditor(task)}>Edit</button>
+                  &nbsp;
+                  <button onClick={() => deleteHandler(task, catKey)}>
+                    Delete
+                  </button>
+                </span>
+              )}
+            </p>
+          </>
         ))}
       </div>
     </div>
--- src/pages/Trello/Trello.tsx
+++ src/pages/Trello/Trello.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { tasksList } from "./tasks";
-import { Category, TaskList } from "../../types/Tasks";
+import { Category, Task, TaskList } from "../../types/Tasks";
 import "./trello.css";
 import { TaskComp } from "./Task";
 
@@ -14,9 +14,35 @@ const Trello = () => {
   // console.log("Task List: ", taskList);
   // <div className="task-container">
 
-  const editHandler = () => {};
+  const addHandler = (task: Task, catKey: Category) => {
+    const taskListCopy = structuredClone(taskList) as TaskList;
+    (taskListCopy as TaskList)[catKey]?.push(task);
+    setTaskList(taskListCopy);
+  };
 
-  const deleteHandler = () => {};
+  const editHandler = (task: Task, catKey: Category) => {
+    if (task?.id) {
+      const taskListCopy = structuredClone(taskList);
+      (taskListCopy as TaskList)[catKey]?.map((currTask) => {
+        if (currTask.id === task.id) {
+          currTask.name = task.name;
+        }
+      });
+      setTaskList(taskListCopy);
+    }
+  };
+
+  const deleteHandler = (task: Task, catKey: Category) => {
+    const c = confirm("Delete this task?");
+    if (!c) return;
+    if (task?.id) {
+      const taskListCopy = structuredClone(taskList) as TaskList;
+      taskListCopy[catKey] = taskListCopy[catKey].filter(
+        (t) => t.id !== task.id
+      );
+      setTaskList(taskListCopy);
+    }
+  };
 
   return (
     <div className="task-container">
@@ -24,8 +50,10 @@ const Trello = () => {
         Object.keys(taskList)?.length > 0 &&
         Object.keys(taskList).map((catKey) => (
           <TaskComp
+            key={catKey}
             catKey={catKey as Category}
             taskList={taskList}
+            addHandler={addHandler}
             editHandler={editHandler}
             deleteHandler={deleteHandler}
           />
